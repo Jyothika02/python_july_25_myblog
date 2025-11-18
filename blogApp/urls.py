cea588a9384776c82280f6774020fc38cb2b249e
Jyothika02 <jyothikababy14@gmail.com>
2025-11-18 22:29:26 +0530

    added

--- blogApp/urls.py
+++ blogApp/urls.py
@@ -1,16 +1,38 @@
+# from django.urls import path
+# from . import views
+
+# urlpatterns = [
+#     path('', views.blog_list, name='blog_list'),
+#     path('blog/<int:blog_id>/', views.blog_detail, name='blog_detail'),
+#     path('create/', views.create_blog, name='create_blog'),
+#     path('myblogs/', views.my_blogs, name='my_blogs'),
+#     path('update/<int:pk>/', views.update_blog, name='update_blog'),
+#     path('delete/<int:pk>/', views.delete_blog, name='delete_blog'),
+#     path('signup/', views.signup_view, name='signup'),
+#     path('logout/', views.logout_view, name='logout'),
+#     path('profile/', views.profile_view, name='profile'),
+#     path('edit/<int:blog_id>/', views.edit_blog, name='edit_blog'), 
+#     # path('blog/<int:pk>/edit/', views.blog_edit, name='blog_edit'),
+# ]
+
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.blog_list, name='blog_list'),
+
+    # Blog detail & comments
     path('blog/<int:blog_id>/', views.blog_detail, name='blog_detail'),
+
+    # Writer actions
     path('create/', views.create_blog, name='create_blog'),
     path('myblogs/', views.my_blogs, name='my_blogs'),
-    path('update/<int:pk>/', views.update_blog, name='update_blog'),
-    path('delete/<int:pk>/', views.delete_blog, name='delete_blog'),
+    path('edit/<int:blog_id>/', views.edit_blog, name='edit_blog'),
+    path('delete/<int:blog_id>/', views.delete_blog, name='delete_blog'),
+
+    # Auth
     path('signup/', views.signup_view, name='signup'),
     path('logout/', views.logout_view, name='logout'),
     path('profile/', views.profile_view, name='profile'),
-    path('edit/<int:blog_id>/', views.edit_blog, name='edit_blog'), 
-    # path('blog/<int:pk>/edit/', views.blog_edit, name='blog_edit'),
 ]
